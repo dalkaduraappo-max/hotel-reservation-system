6997a3447d8843be88144b729e3e867b83d1d98b
unknown <dalkaduraappo@gmail.com>
2026-07-13 11:43:30 +0530

    task 03 | Implement get by id API

---
+++ src/main/java/com/hotel/booking/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.hotel.booking.exception;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+
+    public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex
+                                                        , HttpServletRequest reg){
+        log.info("Not Found {}: {}",reg.getRequestURI(),ex.getMessage());
+
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                new ErrorResponse(404,"Not Found",ex.getMessage())
+        );
+
+
+    }
+}
--- src/main/java/com/hotel/booking/service/RoomService.java
+++ src/main/java/com/hotel/booking/service/RoomService.java
@@ -1,7 +1,7 @@
 package com.hotel.booking.service;
 
-
 import com.hotel.booking.domain.Room;
+import com.hotel.booking.exception.ResourceNotFoundException;
 import com.hotel.booking.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,15 +9,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
-@Service
 @RequiredArgsConstructor
 @Slf4j
+@Service
 public class RoomService {
+
     private final RoomRepository roomRepository;
 
-    @Transactional(readOnly = true)
-    public List<Room> getAllRooms(){
-        return roomRepository.findAll();
+
+    @Transactional(readOnly = true) // Indicates that this method should be run within a read-only transaction
+    public List<Room> getAllRooms() { // Method signature to get all rooms
+        return roomRepository.findAll(); // Returns all records from the room repository
     }
+
+
+    @Transactional(readOnly = true)
+    public Room getById(Long id) {
+        return roomRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Room not found with id - " + id));
+    }
+
+
 }
